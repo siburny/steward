cfd78836b55be819914325b75bf8752c46de54f7
Maxim Rubis <siburny@gmail.com>
2017-04-30 10:49:20 -0400

    update domain name

--- platforms/windows/generate_key.js
+++ platforms/windows/generate_key.js
@@ -1,8 +1,8 @@
-require('x509-keygen').x509_keygen({ subject    : '/CN=steward'
+require('x509-keygen').x509_keygen({ subject    : '/CN=home.local'
                                    , keyfile    : '../../steward/db/server.key'
                                    , certfile   : '../../steward/sandbox/server.crt'
                                    , sha1file   : '../../steward/sandbox/server.sha1'
-                                   , alternates : [ 'DNS:' + require('os').hostname(), 'DNS:steward.local' ]
+                                   , alternates : [ 'DNS:steward', 'DNS:steward.local' ]
                                    , destroy    : false }, function(err, data) {
   if (err) return console.log('keypair generation error: ' + err.message);
 
